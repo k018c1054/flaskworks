05ea568f2424009a4661fcc61a95ad646e2eeebd
k018c1054 <k018c1054@it-neec.jp>
2020-08-04 02:12:51 +0900

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './atatic/uploads/'
 
 @app.route('/')
+def index():
+  return render_template('index.html')
 
-def hello_world():
-  return render_template('index.html', title="タイトル")
+@app.route('/send', methods=['POST'])
+def send():
+  img_file = request.files['img_file']
+  img_file.save(app.config['UPLOAD_FOLDER'] + img_file.filename)
+  return '<p>??' + img_file.filename + '???????</p>'
 
 if __name__=='__main__':
   app.debug = True
