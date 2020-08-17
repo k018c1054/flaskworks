a93197b2a98b3a1da3520c1bce54c2d8bed9f9f4
k018c1054 <k018c1054@it-neec.jp>
2020-08-17 11:58:10 +0900

    kadai5-2.py

--- kadai5-2.py
+++ kadai5-2.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 
 steps = []
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
- dt = datetime.datetime.now()
- date = dt.strftime("%m/%d %H:%M")
- step = request.form.get('step')
- if step is not None:
-   steps.append((date, step))
+  dt = datetime.datetime.now()
+  date = dt.strftime("%m/%d %H:%M")
+  step = request.form.get('step')
+  if step is not None:
+    steps.append((date, step))
   return render_template('kadai5-2.html', steps=steps)
 
 if __name__=='__main__':
